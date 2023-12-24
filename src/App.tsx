a9779a4089a7002dc33fc9b83898fd803faec6f5
anilates97 <anilates.97@gmail.com>
2023-12-24 18:32:26 +0300

    navbar styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import CustomNavBar from "./features/Navbar/Navbar";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <CustomNavBar />
     </div>
   );
