08e84e38abb5c88e8b36c008c5fc8a1d196a21ac
anilates97 <anilates.97@gmail.com>
2023-12-25 05:42:25 +0300

    banner section completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import CustomNavBar from "./features/Navbar/Navbar";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "./features/Banner/Banner";
 
 function App() {
   return (
     <div className="App">
       <CustomNavBar />
+      <Banner />
     </div>
   );
 }
