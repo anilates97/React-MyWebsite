a621aa196be0087a2560d86ff020ba89c9d59ba2
anilates97 <anilates.97@gmail.com>
2024-01-02 16:15:52 +0300

    bug fixed in server side

--- api/mailHandler.js
+++ api/mailHandler.js
@@ -1,7 +1,10 @@
 const nodemailer = require("nodemailer");
-import "dotenv/config";
+require("dotenv").config();
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   const contactEmail = nodemailer.createTransport({
     service: "gmail",
     auth: {
