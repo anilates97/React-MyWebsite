42018c8eab8d49047f7099f7057e83d9c7e21dcb
anilates97 <anilates.97@gmail.com>
2024-01-02 23:52:41 +0300

    server side test

--- server/server.js
+++ server/server.js
@@ -7,10 +7,12 @@ require("dotenv").config();
 
 const app = express();
 
+app.listen(process.env.PORT, () =>
+  console.log("Server Running", process.env.PORT)
+);
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
