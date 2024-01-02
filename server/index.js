3148ac8604d8221ebbbabcbdefcf15cf063ef1ad
anilates97 <anilates.97@gmail.com>
2024-01-03 01:53:50 +0300

    index js updated

--- server/index.js
+++ server/index.js
@@ -1,39 +1,42 @@
 const nodemailer = require("nodemailer");
+const cors = require("cors")({ origin: true });
 
 module.exports = async (req, res) => {
-  if (req.method === "GET") {
-    res.status(200).send("Server is readyy!");
-  } else if (req.method === "POST") {
-    const { firstName, lastName, email, message, phone } = req.body;
-    const name = firstName + lastName;
+  cors(req, res, async () => {
+    if (req.method === "GET") {
+      res.status(200).send("Server is ready!");
+    } else if (req.method === "POST") {
+      const { firstName, lastName, email, message, phone } = req.body;
+      const name = firstName + lastName;
 
-    const contactEmail = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
+      const contactEmail = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
 
-    const mail = {
-      from: name,
-      to: "********@gmail.com",
-      subject: "Contact Form Submission - Portfolio",
-      html: `<p>Name: ${name}</p>
-             <p>Email: ${email}</p>
-             <p>Phone: ${phone}</p>
-             <p>Message: ${message}</p>`,
-    };
+      const mail = {
+        from: name,
+        to: "********@gmail.com",
+        subject: "Contact Form Submission - Portfolio",
+        html: `<p>Name: ${name}</p>
+               <p>Email: ${email}</p>
+               <p>Phone: ${phone}</p>
+               <p>Message: ${message}</p>`,
+      };
 
-    try {
-      await contactEmail.sendMail(mail);
-      res.status(200).json({ code: 200, status: "Message Sent" });
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "An error occurred while sending the email" });
+      try {
+        await contactEmail.sendMail(mail);
+        res.status(200).json({ code: 200, status: "Message Sent" });
+      } catch (error) {
+        res
+          .status(500)
+          .json({ error: "An error occurred while sending the email" });
+      }
+    } else {
+      res.status(405).end();
     }
-  } else {
-    res.status(405).end();
-  }
+  });
 };
